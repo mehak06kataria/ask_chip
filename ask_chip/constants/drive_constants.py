929401c7dd255ad3b94333c8c90b6a3726aa1070
Mehak Kataria <55939602+mehak06kataria@users.noreply.github.com>
2022-07-01 14:15:17 +0530

    Create drive_constants.py

---
+++ ask_chip/constants/drive_constants.py
@@ -0,0 +1,3 @@
+DRIVE_DOC_MIMETYPE = "application/vnd.google-apps.document"
+DRIVE_SHEET_MIMETYPE = "application/vnd.google-apps.spreadsheet"
+FOLDER_MIME_TYPE = 'application/vnd.google-apps.folder'
